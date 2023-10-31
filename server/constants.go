7a45767f655d4a3fbf1fd874a0eeab826f183fb2
Marcel Palaj <palaj.marcel@gmail.com>
2023-10-31 15:44:01 +0100

    finalize trading

--- server/constants.go
+++ server/constants.go
@@ -17,3 +17,6 @@ const LOOT = "LOOT"
 const SHOOT = "SHOOT"
 const BUY = "BUY"
 const STORE = "STORE"
+
+var BASE_PRODUCTION = [...]int{10, 5, 2, 1, 3, 2, 5, 3, 0}
+var BASE_PRICE = [...]int{1, 2, 5, 10, 3, 5, 2, 3, 0}
