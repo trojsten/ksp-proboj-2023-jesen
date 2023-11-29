fd9d3fe2ee6695ea84bf08d385880fe2850111da
MartinBellus <martin.bellus.4@gmail.com>
2023-11-29 15:28:48 +0100

    small fix

--- players/cpp/player.cpp
+++ players/cpp/player.cpp
@@ -7,13 +7,15 @@ World world;
 vector<Turn> do_turn(){
     // sem ide vas kod
     vector<Turn> turns;
-    for(Ship ship : world.my_ships){
+    for(Ship ship : world.my_ships()){
         if(ship.coords == world.my_base && ship.resources[ResourceEnum::Gold] == 0)
             turns.push_back(StoreTurn(ship.index,-5));
         else
+			// takto sa pohybuje
             turns.push_back(MoveTurn(ship.index,{50,50}));
     }
     if(world.my_ships.size() < 3)
+		// takto mozes kupit lod
         turns.push_back(BuyTurn(ShipsEnum::Cln))
     cerr << "Takto mozete vypisovat do logov" << endl;
     cerr << turns << endl;
